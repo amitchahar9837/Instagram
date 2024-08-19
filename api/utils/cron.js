c6627646b80879a59eab82ddf66750fa7bcf0599
AmitChahar <145735414+amitchahar9837@users.noreply.github.com>
2024-08-19 17:55:03 +0530

    Update cron.js
    Update cron job tims

--- api/utils/cron.js
+++ api/utils/cron.js
@@ -4,7 +4,7 @@ import https from 'https';
 const URL = 'https://instagram-cpns.onrender.com';
 
 // Set the cron job to run every 1 hour
-const job = new cron.CronJob("0 * * * *", () => {
+const job = new cron.CronJob("*/20 * * * *", () => {
       https.get(URL, (res) => {
             if (res.statusCode === 200) {
                   console.log("GET request sent successfully");
