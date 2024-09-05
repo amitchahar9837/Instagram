aad81f278b95b5766210ad54d38751e3e5fdb421
AmitChahar <145735414+amitchahar9837@users.noreply.github.com>
2024-09-05 21:10:19 +0530

    Update Signin.jsx

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -57,7 +57,7 @@ export default function Signin() {
           className="border border-gray-300 py-2 px-4 outline-none w-full focus:border-blue-500 rounded-md"
           type="text"
           placeholder="username"
-          defaultValue="test_123"
+          defaultValue="guest1234"
           id="username"
           onChange={handleChange}
         />
@@ -66,7 +66,7 @@ export default function Signin() {
           className="border border-gray-300 py-2 px-4 outline-none w-full focus:border-blue-500 rounded-md"
           type={`${showPassword ? "text" : "password"}`}
           id="password"
-          defaultValue="test@123"
+          defaultValue="guest1234"
           placeholder="••••••••"
           onChange={handleChange}
         />
